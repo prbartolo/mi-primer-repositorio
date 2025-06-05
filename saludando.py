4dcdfd48c0d9a1f9899dea4bebe03937ae27774e
prbartolo <pbartolobaquerizo@gmail.com>
2025-06-04 20:02:14 -0500

    Diccionario.py

--- saludando.py
+++ saludando.py
@@ -1,1 +1,11 @@
-print("hola a todos")
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+if word in meme_dict.keys():
+    # ¿Qué debemos hacer si se encuentra la palabra?
+else:
+    # ¿Qué hacer si no se encuentra la palabra?
+print(meme_dict["CRINGE"])
+
